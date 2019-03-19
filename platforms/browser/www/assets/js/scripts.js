0a9ce778fd4c415c913122cf6d882d71b14b2e62
kenzo44 <kenzo44@users.noreply.github.com>
2019-03-19 11:00:18 -0600

    🚀Initial Dump

---
+++ platforms/browser/www/assets/js/scripts.js
@@ -0,0 +1,40 @@
+var app = new Framework7({
+    root: "#app", //this is the app element
+    routes: [
+        {
+            path: '/page2/',
+            url: 'pages/page2.html'
+        }
+    ]
+})
+
+var mainView = app.views.create('.view-main');
+
+//For Plugins
+document.addEventListener("deviceready", init, false);
+
+function init() {
+    //camera code goes here
+
+    $("#takePic").on("click", function () {
+        console.log("yaye");
+        navigator.camera.getPicture(success, error, cameraOptions);
+
+    });
+    
+    var cameraOptions = {
+        //Default 50 range 0-100
+        quality: 80
+
+
+    };
+
+    function success(imageURI) {
+        console.log("great pic");
+        $("#takePic").after("<img src='" + imageURI + "'>");
+    }
+
+    function error(message) {
+        alert("failiure due to: " + message);
+    }
+}
